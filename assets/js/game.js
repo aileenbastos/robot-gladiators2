4c1a65427b60e95ede7ee6f5ccc3e11038c5fd9a
davidsdottir <aileen.bastos@gmail.com>
2021-08-13 10:53:04 -0400

    debugged single enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,10 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
+  while(enemyHealth > 0) {
+
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  // window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -72,7 +74,10 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 };
+};
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
